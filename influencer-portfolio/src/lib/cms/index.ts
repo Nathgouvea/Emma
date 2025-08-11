a879be512a7f84c8932245874055e095992652f2
Nathgouvea <132577952+Nathgouvea@users.noreply.github.com>
2025-08-11 09:13:40 +0100

    reset project

--- influencer-portfolio/src/lib/cms/index.ts
+++
@@ -1,9 +0,0 @@
-import local from "./local";
-import type { CmsProvider } from "./types";
-
-export function getCms(): CmsProvider {
-  // In the future, switch based on env CMS_PROVIDER
-  return local;
-}
-
-export * from "./types";
